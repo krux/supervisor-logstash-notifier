439f896a7efa9b98c25c4e322c0cfbd16e86184e
Alexander O'Donovan-Jones <alexander@dohop.com>
2016-02-16 14:31:43 +0000

    Rename the test file

--- tests/test_syslog_notifier.py
+++ tests/test_syslog_notifier.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2014  Infoxchange Australia
+# Copyright 2016 Dohop hf.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 """
-Test supervisor_logging.
+Test syslog_notifier
 """
 
 import os
