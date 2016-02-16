768eddc9a255c4840722e1335a09c18d1ba59912
Alexander O'Donovan-Jones <alexander@dohop.com>
2016-02-16 13:42:55 +0000

    Adding reqs.txt to the setup, along with reading of the README for the long_description

--- setup.py
+++ setup.py
@@ -16,18 +16,21 @@
 
 from setuptools import setup
 
-setup(
-	name='supervisord-syslog-notifier',
-	version='0.0.1',
-	packages=['syslog_notifier'],
-	url='https://github.com/dohop/supervisord-syslog-notifier',
-	license='Apache 2.0',
-	author='alexander',
-	author_email='alexander@dohop.com',
-	description='Stream supervisord events to a logstash/syslog instance',
-	entry_points={
-		'console_scripts': [
-			'syslog_notifier = syslog_notifier:main'
-		]
-	},
-)
+with open('requirements.txt') as requirements:
+	setup(
+		name='supervisord-syslog-notifier',
+		version='0.0.1',
+		packages=['syslog_notifier'],
+		url='https://github.com/dohop/supervisord-syslog-notifier',
+		license='Apache 2.0',
+		author='alexander',
+		author_email='alexander@dohop.com',
+		description='Stream supervisord events to a logstash/syslog instance',
+		long_description=open('README.md').read(),
+		entry_points={
+			'console_scripts': [
+				'syslog_notifier = syslog_notifier:main'
+			]
+		},
+		install_requires=requirements.read().splitlines(),
+	)
